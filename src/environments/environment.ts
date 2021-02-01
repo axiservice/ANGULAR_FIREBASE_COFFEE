72e32fe6a48f79b1e4eac0aad6ff3bf8f0efd831
axiservice <axi.tms@gmail.com>
2021-02-01 21:24:15 +0100

    nn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBKhVDuTZEDlZFqMj_aW7-otDbk87e2dqs",
-    authDomain: "provacontainer001.firebaseapp.com",
-    databaseURL: "https://provacontainer001-default-rtdb.firebaseio.com",
-    projectId: "provacontainer001",
-    storageBucket: "provacontainer001.appspot.com",
-    messagingSenderId: "595800346753",
-    appId: "1:595800346753:web:b16f29d6b4d632c2a67d80",
-    measurementId: "G-VXC33SX5GQ"
+
   }
 };
 
